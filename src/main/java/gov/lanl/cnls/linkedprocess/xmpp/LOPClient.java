179ae9a3883031de89ca385b88febaad2d086bfb
okrammarko <okrammarko@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-06-23 17:07:36 +0000

    created the stub for the LOP client designed yesterday with Josh.

--- src/main/java/gov/lanl/cnls/linkedprocess/xmpp/LOPClient.java
+++ src/main/java/gov/lanl/cnls/linkedprocess/xmpp/LOPClient.java
@@ -1,26 +1,37 @@
-package gov.lanl.cnls.linkedprocess;
+package gov.lanl.cnls.linkedprocess.xmpp;
 
-import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Presence;
+import gov.lanl.cnls.linkedprocess.GenericPacketListener;
 
-public class SmackPlay {
-    
-    // this is a development and testing account. please don't use for any other purposes.
+/**
+ * Created by IntelliJ IDEA.
+ * User: marko
+ * Date: Jun 23, 2009
+ * Time: 11:01:06 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LOPClient {
+
+        // this is a development and testing account. please don't use for any other purposes.
     private static String username = "linked.process.1@gmail.com";
     private static String password = "linked12";
     private static String resource = "lop/";
-        
-    public static void main(String[] args) throws Exception {
+    private static String xmpp_server = "talk1.l.google.com";
+    private static int xmpp_port = 5222;
 
-        // this will load a Java Swing GUI and allow you to view and send XMPP XML packets/stanzas
-        XMPPConnection.DEBUG_ENABLED = true;
+    public static void main(String[] args) throws Exception {
+        new LOPClient();
+    }
+
+    public LOPClient() throws Exception {
 
         System.out.println("Starting client");
-        ConnectionConfiguration connConfig = new ConnectionConfiguration("talk1.l.google.com", 5222);
+        ConnectionConfiguration connConfig = new ConnectionConfiguration(xmpp_server, xmpp_port);
         XMPPConnection connection = new XMPPConnection(connConfig);
-        
+
         // making a connection to an XMPP server is different than logging into an XMPP server
         try {
             connection.connect();
@@ -34,10 +45,10 @@ public class SmackPlay {
             connection.login(username, password, resource);
             System.out.println("Logged in as " + connection.getUser());
             // acknowledge to all "buddies" your presence. This presence uses a custom message and a priority value.
-            Presence presence = new Presence(Presence.Type.available,
-                    "Waiting to process...", 24, Presence.Mode.available);
-            connection.sendPacket(presence);
-            
+            //Presence presence = new Presence(Presence.Type.available,
+            //        "Waiting to process...", 24, Presence.Mode.available);
+            //connection.sendPacket(presence);
+
         } catch (XMPPException ex) {
             System.out.println("Failed to log in as " + username);
             System.out.println(ex);
@@ -53,13 +64,12 @@ public class SmackPlay {
         System.out.println("Compression: " + connection.isUsingCompression());
         System.out.println("Transport Layer Security: " + connection.isUsingTLS());
         System.out.println();
-        
+
 
         // Smack uses a listener framework to receive packets/stanzas and perform operations on packets.
         // A listener can have a filter to only handle certain types of packets (tags/attributes)
         //PacketFilter filter = new PacketTypeFilter(IQ.class);
         connection.addPacketListener(new GenericPacketListener(), null);
-       
+
     }
 }
-
