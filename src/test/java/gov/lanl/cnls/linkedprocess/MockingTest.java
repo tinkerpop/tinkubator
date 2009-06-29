46c7f8ddd19d0ca816d8558892b10acaacbc7a85
neubauer.peter <neubauer.peter@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-06-29 12:27:17 +0000

    uncovering the first error with a failing test, I cannot start 2 LoPVMs in the same farm.

--- src/test/java/gov/lanl/cnls/linkedprocess/MockingTest.java
+++ src/test/java/gov/lanl/cnls/linkedprocess/MockingTest.java
@@ -27,6 +27,7 @@ import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.filter.PacketFilter;
 import org.jivesoftware.smack.packet.IQ;
 import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
 import org.jivesoftware.smackx.ServiceDiscoveryManager;
 import org.junit.Before;
 import org.junit.Test;
@@ -51,19 +52,22 @@ public class MockingTest {
 	private List<PacketListener> packetListeners;
 	private String mockFarmId;
 	private String mockClient;
+	private Packet spawnPacket;
+	private String spawnPacketId;
+	private XMPPConnection mockConnection;
 
 	@Before
 	public void setup() throws Exception {
 		ServiceDiscoveryManager mdm = createMock(ServiceDiscoveryManager.class);
-		XMPPConnection mockConnection = createMock(XMPPConnection.class);
+		mockConnection = createMock(XMPPConnection.class);
 		mockStatic(ServiceDiscoveryManager.class);
 		expect(ServiceDiscoveryManager.getInstanceFor(mockConnection))
 				.andReturn(mdm).anyTimes();
 		Iterator<String> features = new LinkedList<String>().iterator();
 		expect(mdm.getFeatures()).andReturn(features).anyTimes();
 		mdm.addFeature(isA(String.class));
-		// 2 for Farm, 2 for VM
-		expectLastCall().times(4);
+		// 2 for Farm, 2 for each VM
+		expectLastCall().anyTimes();
 		// a bit more explicit matchers to help PowerMock choose the right
 		// constructor
 		expectNew(XMPPConnection.class,
@@ -72,8 +76,8 @@ public class MockingTest {
 				.anyTimes();
 
 		mockConnection.connect();
-		// one time for the XMPPFarm, one time for the spawned VM
-		expectLastCall().times(2);
+		// one time for the XMPPFarm, one time for each spawned VM
+		expectLastCall().anyTimes();
 		// just connection info, not important right now
 		expect(mockConnection.getHost()).andReturn("mock.linkedprocess.org")
 				.anyTimes();
@@ -87,7 +91,7 @@ public class MockingTest {
 		packetListeners = new LinkedList<PacketListener>();
 		mockConnection.addPacketListener(MultiCaptureMatcher
 				.multiCapture(packetListeners), isA(PacketFilter.class));
-		expectLastCall().times(6);
+		expectLastCall().anyTimes();
 		Roster mockRoster = createMock(Roster.class);
 		mockRoster.setSubscriptionMode(Roster.SubscriptionMode.manual);
 		expect(mockConnection.getRoster()).andReturn(mockRoster).anyTimes();
@@ -97,20 +101,22 @@ public class MockingTest {
 		expectLastCall().anyTimes();
 		mockConnection.login(isA(String.class), isA(String.class),
 				isA(String.class));
-		expectLastCall().times(2);
-	}
-
-	@Test
-	public void testMockingXMPP() throws Exception {
-
-		// create a spawn packet for later injection
-		Packet spawnPacket = createMock(Packet.class);
+		expectLastCall().anyTimes();
+		//shutdown
+		mockConnection.disconnect(isA(Presence.class));
+		mockConnection.disconnect();
+		spawnPacket = createMock(Packet.class);
 		String spawnXML = "<not real XML>";
 		expect(spawnPacket.toXML()).andReturn(spawnXML).anyTimes();
 		mockClient = "mockClientUser";
 		expect(spawnPacket.getFrom()).andReturn(mockClient).anyTimes();
-		String spawnPacketId = "123";
+		spawnPacketId = "123";
 		expect(spawnPacket.getPacketID()).andReturn(spawnPacketId).anyTimes();
+
+	}
+
+	@Test
+	public void testSpawningOneVM() throws Exception {
 
 		// activate all mock objects
 		replayAll();
@@ -144,6 +150,51 @@ public class MockingTest {
 
 		// now we should have 3 more PacketListeners for the VM
 		assertTrue(packetListeners.size() == 6);
+
+		//shut down
+		xmppFarm.shutDown();
+		verifyAll();
+	}
+
+	@Test
+	public void testSpawningTwoVMs() throws Exception {
+		Packet spawnPacket2 = createMock(Packet.class);
+		String spawnXML = "<not real XML>";
+		expect(spawnPacket2.toXML()).andReturn(spawnXML).anyTimes();
+		mockClient = "mockClientUser";
+		expect(spawnPacket2.getFrom()).andReturn(mockClient).anyTimes();
+		String spawnPacket2Id = "345";
+		expect(spawnPacket2.getPacketID()).andReturn(spawnPacket2Id).anyTimes();
+		// activate all mock objects
+		replayAll();
+
+		// start testing
+		XmppFarm xmppFarm = new XmppFarm(server, port, username1, password1);
+
+		PacketListener spawnListener = packetListeners.get(0);
+
+		// let's send a spawn packet!
+		spawnListener.processPacket(spawnPacket);
+		// let's send one more spawn packet and fire up one more VM!
+		spawnListener.processPacket(spawnPacket2);
+		// now, a new packet should have been sent back from the VM
+		assertEquals(5, sentPackets.size());
+		// sent packet should refer to the same pID
+		IQ result = (IQ) sentPackets.get(4);
+		assertEquals(result.getPacketID(), spawnPacket2Id);
+		assertEquals(IQ.Type.RESULT, result.getType());
+		// check the whole xml string
+		assertEquals("<iq id=\"" + spawnPacket2Id + "\" to=\"" + mockClient
+				+ "\" type=\"result\"><" + Spawn.SPAWN_TAGNAME + " xmlns=\""
+				+ LinkedProcess.LOP_FARM_NAMESPACE + "\" "
+				+ Spawn.VM_JID_ATTRIBUTE + "=\"" + mockFarmId + "\" /></iq>",
+				result.toXML());
+
+		// now we should have 3 more PacketListeners for the VM
+		assertTrue(packetListeners.size() == 6);
+
+		//shut down
+		xmppFarm.shutDown();
 		verifyAll();
 	}
 }
