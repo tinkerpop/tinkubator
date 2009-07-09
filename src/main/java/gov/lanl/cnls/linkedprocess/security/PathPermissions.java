a85f5bd68039089d59023b867f33a4e8baad0192
parcour <parcour@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-07-09 22:56:33 +0000

    Added a data structure for path-based permissions.

---
+++ src/main/java/gov/lanl/cnls/linkedprocess/security/PathPermissions.java
@@ -0,0 +1,109 @@
+package gov.lanl.cnls.linkedprocess.security;
+
+import java.util.Collection;
+import java.util.LinkedList;
+
+/**
+ * Author: josh
+ * Date: Jul 9, 2009
+ * Time: 3:19:15 PM
+ */
+public class PathPermissions {
+    private static final Boolean
+            PERMIT = true,
+            DENY = false;
+
+    private final Node<Boolean> rootNode;
+
+    public PathPermissions() {
+        rootNode = new Node<Boolean>("", DENY);
+    }
+
+    public void addPermitRule(final String path) {
+        rootNode.addChild(path, PERMIT);
+    }
+
+    public void addDenyRule(final String path) {
+        rootNode.addChild(path, DENY);
+    }
+
+    public boolean isPermitted(final String path) {
+        Boolean b = rootNode.findTarget(path);
+
+        // Note: this check for null is not really necessary...
+        return null == b ? false : b;
+    }
+
+    private class Node<T> implements Comparable<Node<T>> {
+        private final String prefix;
+        private final T target;
+        private final Collection<Node<T>> children;
+
+        public Node(final String prefix,
+                    final T target) {
+            this.prefix = prefix;
+            this.target = target;
+            children = new LinkedList<Node<T>>();
+        }
+
+        public T findTarget(final String s) {
+            if (s.startsWith(prefix)) {
+                String suffix = s.substring(prefix.length());
+
+                // Children are tested first.
+                for (Node<T> child : children) {
+                    T t = child.findTarget(suffix);
+                    if (null != t) {
+                        return t;
+                    }
+                }
+
+                // If no child applies, the node's own target is used.
+                return target;
+            } else {
+                // Node does not apply.
+                return null;
+            }
+        }
+
+        public void addChild(final String suffix,
+                             final T target) {
+            removeSupersededChildren(suffix);
+
+            for (Node<T> child : children) {
+                // If the new child modifies (but does not supersede) an existing child...
+                if (suffix.startsWith(child.prefix)) {
+                    // New suffix will be non-empty.
+                    String newSuffix = suffix.substring(child.prefix.length());
+
+                    child.addChild(newSuffix, target);
+
+                    // Child will not modify more than one existing child.
+                    return;
+                }
+            }
+
+            // If no children are modified, simply add a new child.
+            children.add(new Node<T>(suffix, target));
+        }
+
+        private void removeSupersededChildren(final String prefix) {
+            // Note: it is possible for more than one existing child to be superseded by a new child.
+            Collection<Node<T>> superseded = new LinkedList<Node<T>>();
+
+            for (Node<T> child : children) {
+                if (child.prefix.startsWith(prefix)) {
+                    superseded.add(child);
+                }
+            }
+
+            for (Node<T> child : superseded) {
+                children.remove(child);
+            }
+        }
+
+        public int compareTo(final Node<T> other) {
+            return this.prefix.compareTo(other.prefix);
+        }
+    }
+}
---
+++ src/test/java/gov/lanl/cnls/linkedprocess/security/PathPermissionsTest.java
@@ -0,0 +1,59 @@
+package gov.lanl.cnls.linkedprocess.security;
+
+import junit.framework.TestCase;
+
+/**
+ * Author: josh
+ * Date: Jul 9, 2009
+ * Time: 3:31:20 PM
+ */
+public class PathPermissionsTest extends TestCase {
+    public void testDefaultToDeny() {
+        PathPermissions p = new PathPermissions();
+
+        p.addPermitRule("a");
+        assertFalse(p.isPermitted("b"));
+    }
+
+    public void testInheritance() {
+        PathPermissions p = new PathPermissions();
+
+        p.addPermitRule("a");
+        assertTrue(p.isPermitted("aa"));
+    }
+
+    public void testEmptyRoot() {
+        PathPermissions p = new PathPermissions();
+
+        p.addPermitRule("");
+        assertTrue(p.isPermitted("a"));
+
+        p.addDenyRule("");
+        assertFalse(p.isPermitted("a"));
+    }
+
+    public void testNewRulesOverride() {
+        PathPermissions p = new PathPermissions();
+
+        p.addPermitRule("aa");
+        assertTrue(p.isPermitted("aa"));
+        assertTrue(p.isPermitted("aabc"));
+        assertFalse(p.isPermitted("a"));
+
+        p.addDenyRule("aab");
+        assertTrue(p.isPermitted("aa"));
+        assertFalse(p.isPermitted("aabc"));
+    }
+
+    public void testNewRulesOverrideChildren() {
+        PathPermissions p = new PathPermissions();
+
+        p.addPermitRule("aa");
+        assertTrue(p.isPermitted("aacd"));
+        p.addDenyRule("aac");
+        assertFalse(p.isPermitted("aacd"));
+
+        p.addPermitRule("aa");
+        assertTrue(p.isPermitted("aacd"));
+    }
+}
