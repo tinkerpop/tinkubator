1aa5b0d9e2abcb65df1863e2aa2961e6ed30eea1
neubauer.peter <neubauer.peter@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-07-27 19:10:13 +0000

    reading up props

--- laboratory/peter/villein-15-app/src/main/java/org/linkedprocess/villein/android/Villein.java
+++ laboratory/peter/villein-15-app/src/main/java/org/linkedprocess/villein/android/Villein.java
@@ -1,13 +1,32 @@
 package org.linkedprocess.villein.android;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import android.app.Activity;
 import android.os.Bundle;
 
 public class Villein extends Activity {
-    /** Called when the activity is first created. */
+    private static final String PROPETIES = "defaults.properties";
+	private Bundle savedInstanceState;
+
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        this.savedInstanceState = savedInstanceState;
+		super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        try {
+			connect();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
+
+	private void connect() throws IOException {
+		Properties props = new Properties();
+		props.load(getAssets().open(PROPETIES));
+		
+	}
 }
