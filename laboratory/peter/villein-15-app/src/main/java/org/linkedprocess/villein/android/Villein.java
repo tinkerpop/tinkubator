e6d91da55c607dbfe67440193f23dfd7bf949490
neubauer.peter <neubauer.peter@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-07-27 18:39:32 +0000

    checking in a first version of a maven built Android package

---
+++ laboratory/peter/villein-15-app/src/main/java/org/linkedprocess/villein/android/Villein.java
@@ -0,0 +1,13 @@
+package org.linkedprocess.villein.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Villein extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
