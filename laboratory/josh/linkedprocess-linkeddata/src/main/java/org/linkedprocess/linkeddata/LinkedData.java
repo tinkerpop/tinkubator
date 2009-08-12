a48326be5c4d6fab3ced2a5adb867947ac27fb33
parcour <parcour@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-08-12 21:50:02 +0000

    Added Groovy portion of the Linked Data demo.

--- laboratory/josh/linkedprocess-linkeddata/src/main/java/org/linkedprocess/linkeddata/LinkedData.java
+++ laboratory/josh/linkedprocess-linkeddata/src/main/java/org/linkedprocess/linkeddata/LinkedData.java
@@ -1,5 +1,14 @@
 package org.linkedprocess.linkeddata;
 
+import org.linkedprocess.LinkedProcess;
+import org.linkedprocess.LinkedProcessFarm;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 /**
  * Created by IntelliJ IDEA.
  * User: josh
@@ -9,6 +18,26 @@ package org.linkedprocess.linkeddata;
  */
 public class LinkedData {
     public static void main(final String[] args) throws Exception {
-        
+        new LinkedData().doit();
+    }
+
+    private void doit() throws IOException, ScriptException {
+        ScriptEngine engine = LinkedProcessFarm.getScriptEngineManager()
+                .getEngineByName(LinkedProcess.GROOVY);
+
+        String program = toString(LinkedData.class.getResourceAsStream("lddemo.groovy"));
+        Object result = engine.eval(program);
+
+        System.out.println("result = " + result);
+    }
+
+    private String toString(final InputStream is) throws IOException {
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        while (0 < is.available()) {
+            bos.write(is.read());
+        }
+        is.close();
+
+        return bos.toString();
     }
 }
