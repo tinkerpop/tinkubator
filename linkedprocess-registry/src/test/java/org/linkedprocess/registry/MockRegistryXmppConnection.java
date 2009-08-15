66fa121c194ce947a6cd1093e3a50f4da3b23716
neubauer.peter <neubauer.peter@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-08-15 19:01:58 +0000

    more registry tests. XMPP spec level is almost done now

--- linkedprocess-registry/src/test/java/org/linkedprocess/registry/MockRegistryXmppConnection.java
+++ linkedprocess-registry/src/test/java/org/linkedprocess/registry/MockRegistryXmppConnection.java
@@ -1,14 +1,36 @@
 package org.linkedprocess.registry;
 
 import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.filter.PacketFilter;
 import org.linkedprocess.testing.offline.MockXMPPConnection;
+import org.linkedprocess.xmpp.registry.DiscoItemsListener;
+import org.linkedprocess.xmpp.registry.PresenceListener;
+import org.linkedprocess.xmpp.registry.PresenceSubscriptionListener;
 
 public class MockRegistryXmppConnection extends MockXMPPConnection {
+
+	public PacketListener subscription, presence, discoItems;
 
 	public MockRegistryXmppConnection(ConnectionConfiguration connConfig,
 			String id, XMPPConnection connection) {
 		super(connConfig, id, connection);
+	}
+
+	@Override
+	public void addPacketListener(PacketListener listener, PacketFilter filter) {
+		super.addPacketListener(listener, filter);
+		if (listener instanceof PresenceSubscriptionListener) {
+			subscription = listener;
+		}
+		if (listener instanceof PresenceListener) {
+			presence = listener;
+		}
+		if (listener instanceof DiscoItemsListener) {
+			discoItems = listener;
+		}
+
 	}
 
 }
--- linkedprocess-registry/src/test/java/org/linkedprocess/registry/OfflineRegistryTest.java
+++ linkedprocess-registry/src/test/java/org/linkedprocess/registry/OfflineRegistryTest.java
@@ -1,6 +1,8 @@
 package org.linkedprocess.registry;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.powermock.api.easymock.PowerMock.createMock;
 import static org.powermock.api.easymock.PowerMock.replayAll;
 
@@ -9,8 +11,11 @@ import java.util.ArrayList;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.Presence;
+import org.jivesoftware.smackx.packet.DiscoverItems;
 import org.junit.Before;
 import org.junit.Test;
+import org.linkedprocess.LinkedProcess;
 import org.linkedprocess.testing.offline.OfflineTest;
 import org.linkedprocess.xmpp.registry.XmppRegistry;
 
@@ -23,17 +28,94 @@ public class OfflineRegistryTest extends OfflineTest {
 	@Before
 	public void startVM() throws Exception {
 		XMPPConnection registryConn = createMock(XMPPConnection.class);
-		connection = new MockRegistryXmppConnection(new ConnectionConfiguration(
-				server, port), "LoPRegistry", registryConn);
+		connection = new MockRegistryXmppConnection(
+				new ConnectionConfiguration(server, port), "LoPRegistry",
+				registryConn);
 		OfflineTest.prepareMocksAndConnection(registryConn, connection);
 		sentPackets = connection.sentPackets;
 		replayAll();
 		// start the farm
 		reg = new XmppRegistry(server, port, username, password);
 	}
-	
+
 	@Test
 	public void correctStartupAndShutdown() {
 		assertEquals(3, connection.packetListeners.size());
+		assertNotNull(connection.subscription);
+		assertNotNull(connection.presence);
+		assertNotNull(connection.discoItems);
+	}
+
+	@Test
+	public void subscriptionShouldBeOfTypeBoth() {
+		connection.clearPackets();
+		Presence sub = new Presence(Presence.Type.subscribe);
+		sub.setFrom(CLIENT_JID);
+		connection.subscription.processPacket(sub);
+		connection.presence.processPacket(sub);
+		assertEquals(2, connection.sentPackets.size());
+		assertEquals(Presence.Type.subscribed, ((Presence) sentPackets.get(0))
+				.getType());
+		// the subscription request from the registry
+		assertEquals(Presence.Type.subscribe, ((Presence) sentPackets.get(1))
+				.getType());
+		// answer the subscription
+		sub.setType(Presence.Type.subscribed);
+		connection.presence.processPacket(sub);
+	}
+
+	@Test
+	public void presenceFromActiveSubscriptionShouldListCountrysideInRegistryAsActive() {
+
+		// first, subscribe to the registry
+		Presence presence = new Presence(Presence.Type.subscribe);
+		presence.setFrom(CLIENT_JID);
+		connection.subscription.processPacket(presence);
+		connection.presence.processPacket(presence);
+		connection.clearPackets();
+
+		// now, send available from farm
+		presence.setType(Presence.Type.available);
+		connection.presence.processPacket(presence);
+
+		// now, test discovering the farms countryside
+		connection.clearPackets();
+		DiscoverItems di = new DiscoverItems();
+		di.setFrom(CLIENT_JID);
+		connection.discoItems.processPacket(di);
+		assertEquals(1, sentPackets.size());
+		DiscoverItems result = (DiscoverItems) connection.getLastPacket();
+		assertEquals(LinkedProcess.generateBareJid(CLIENT_JID), result
+				.getItems().next().getEntityID());
+		
+		//having the farm unavailable should exclude it from the listing
+		// now, send unavailable from farm
+		presence.setType(Presence.Type.unavailable);
+		connection.presence.processPacket(presence);
+		// now, test discovering the farm
+		connection.clearPackets();
+		connection.discoItems.processPacket(di);
+		assertEquals(1, sentPackets.size());
+		result = (DiscoverItems) connection.getLastPacket();
+		assertFalse(result.getItems().hasNext());
+		
+	}
+
+	@Test
+	public void presenceWithoutSubscriptionShouldWorkToo() {
+
+		connection.clearPackets();
+		Presence presence = new Presence(Presence.Type.available);
+		presence.setFrom(CLIENT_JID);
+		connection.presence.processPacket(presence);
+		connection.clearPackets();
+		DiscoverItems di = new DiscoverItems();
+		di.setFrom(CLIENT_JID);
+		connection.discoItems.processPacket(di);
+		assertEquals(1, sentPackets.size());
+		DiscoverItems result = (DiscoverItems) connection.getLastPacket();
+		assertEquals(LinkedProcess.generateBareJid(CLIENT_JID), result
+				.getItems().next().getEntityID());
+		
 	}
 }
