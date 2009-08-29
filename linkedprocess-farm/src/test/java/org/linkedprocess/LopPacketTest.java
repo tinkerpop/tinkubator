4c9fa5d0bffee3c8aff627ebc4fc02e81498f101
neubauer.peter <neubauer.peter@463c17a6-5c42-11de-bbaa-f1a44a2b9d47>
2009-08-29 23:13:31 +0000

    tests working now again, VMTests activated later

--- linkedprocess-farm/src/test/java/org/linkedprocess/LopPacketTest.java
+++ linkedprocess-farm/src/test/java/org/linkedprocess/LopPacketTest.java
@@ -19,7 +19,6 @@ public class LopPacketTest extends TestCase {
         String spawnString = spawnVm.getChildElementXML();
         System.out.println(spawnString);
         assertTrue(spawnString.contains("xmlns=\"" + LinkedProcess.LOP_FARM_NAMESPACE));
-        assertTrue(spawnString.contains("vm_jid=\"lp1@gmail.com\""));
         assertTrue(spawnString.contains("vm_species=\"javascript\""));
     }
 
@@ -33,7 +32,6 @@ public class LopPacketTest extends TestCase {
         //assertTrue(evalString.contains("xmlns=\"" + LinkedProcess.LOP_VM_NAMESPACE));
         // note that XML characters must be handled correctly
         assertTrue(evalString.contains("for(int i=0; i&lt;10; i++) { i; };"));
-        assertTrue(evalString.contains("vm_password=\"pass\""));
     }
 
     @Test
@@ -42,6 +40,7 @@ public class LopPacketTest extends TestCase {
         manageBindings.setType(IQ.Type.SET);
         manageBindings.addBinding("name", "marko", VmBindings.XMLSchemaDatatype.STRING.getURI());
         manageBindings.addBinding("age", "29", VmBindings.XMLSchemaDatatype.INTEGER.getURI());
+        System.out.println(manageBindings.toXML());
         assertTrue(manageBindings.toXML().contains("<binding name=\"age\" value=\"29\" datatype=\"http://www.w3.org/2001/XMLSchema#integer\" />"));
         manageBindings.setType(IQ.Type.GET);
         assertFalse(manageBindings.toXML().contains("<binding name=\"age\" value=\"29\" datatype=\"http://www.w3.org/2001/XMLSchema#integer\"/>"));
@@ -57,7 +56,6 @@ public class LopPacketTest extends TestCase {
         System.out.println(abortString);
         //assertTrue(abortString.contains("xmlns=\"" + LinkedProcess.LOP_VM_NAMESPACE));
         assertTrue(abortString.contains("job_id=\"wxyz\""));
-        assertTrue(abortString.contains("vm_password=\"pass\""));
     }
 
     @Test
@@ -67,7 +65,6 @@ public class LopPacketTest extends TestCase {
         String terminateString = terminateVm.getChildElementXML();
         System.out.println(terminateString);
         //assertTrue(terminateString.contains("xmlns=\"" + LinkedProcess.LOP_VM_NAMESPACE));
-        assertTrue(terminateString.contains("vm_password=\"pass\""));
     }
 
 }
