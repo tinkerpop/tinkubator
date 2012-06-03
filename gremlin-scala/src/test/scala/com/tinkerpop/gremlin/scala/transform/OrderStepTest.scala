e5a675aa4c25f4a6513d98ee3922e1d00fa6dfa3
Alex Varju <alex@varju.ca>
2012-06-03 11:16:29 -0700

    fix embarrassing compile error in unit tests

--- gremlin-scala/src/test/scala/com/tinkerpop/gremlin/scala/transform/OrderStepTest.scala
+++ gremlin-scala/src/test/scala/com/tinkerpop/gremlin/scala/transform/OrderStepTest.scala
@@ -4,6 +4,7 @@ import com.tinkerpop.blueprints.Vertex
 import com.tinkerpop.blueprints.impls.tg.TinkerGraphFactory
 import com.tinkerpop.gremlin.scala._
 import com.tinkerpop.gremlin.test.ComplianceTest
+import com.tinkerpop.pipes.Pipe
 import com.tinkerpop.pipes.util.structures.Pair
 
 class OrderStepTest extends com.tinkerpop.gremlin.test.transform.OrderStepTest {
@@ -14,15 +15,14 @@ class OrderStepTest extends com.tinkerpop.gremlin.test.transform.OrderStepTest {
   }
 
   def test_g_V_name_order() {
-    super.test_g_V_name_order(
-      g.V.property("name").order())
+    super.test_g_V_name_order(g.V.property("name").asInstanceOf[GremlinScalaPipeline[Vertex, String]].order())
   }
 
   def test_g_V_name_orderXabX() {
-    super.test_g_V_name_orderXabX(g.V.property("name").order({ arg: Pair[String, String] => arg.getB.compareTo(arg.getA) }))
+    super.test_g_V_name_orderXabX(g.V.property("name").asInstanceOf[GremlinScalaPipeline[Vertex, String]].order({ arg: Pair[String, String] => arg.getB.compareTo(arg.getA) }))
   }
 
   def test_g_V_orderXa_nameXb_nameX_name() {
-    super.test_g_V_orderXa_nameXb_nameX_name(g.V.order({ arg: Pair[Vertex, Vertex] => arg.getB.as[String]("name").get.compareTo(arg.getA.as[String]("name").get) }).property("name"))
+    super.test_g_V_orderXa_nameXb_nameX_name(g.V.order({ arg: Pair[Vertex, Vertex] => arg.getB.as[String]("name").get.compareTo(arg.getA.as[String]("name").get) }).property("name").asInstanceOf[GremlinScalaPipeline[Vertex, String]])
   }
 }
